76f4c0227e4d691da9d2af934d28d78f2771b92a
yangjie <18328434957@163.com>
2019-07-30 14:57:55 +0800

    “update test.java

--- src/main/java/com/bruce/Test.java
+++ src/main/java/com/bruce/Test.java
@@ -7,6 +7,5 @@ package com.bruce;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
     }
 }
